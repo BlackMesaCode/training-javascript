742579903aadec47630df8d8fd7c0a6a16c44154
Stephan Sum <sum@live.de>
2017-03-15 00:13:41 +0100

    updated es6 module pattern training #2

--- client/src/module-patterns/es2015/calculator.js
+++ client/src/module-patterns/es2015/calculator.js
@@ -1,4 +1,5 @@
 import { multiply, multThenSum } from './advancedMath.js';
+// module "./advancedMath.js"; // the module syntax is only available in typescript and equals: import * as <moduleName> from "./<moduleName>.js"
 
 console.log("multiply result: " + multiply(2,2));
 console.log("multThenSum result: " + multThenSum(1,2,3));
