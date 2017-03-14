63fbc8c343114329dfb82870dcd70b66914ef1f8
Stephan Sum <sum@live.de>
2017-03-14 12:58:33 +0100

    improved build automation; moved to gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,39 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
- 
-gulp.task('transpile-es2015', () => {
-    return gulp.src('./client/src/module-patterns/es2015/*.js')
-        .pipe(babel({
-            presets: ['es2015']
-        }))
-        .pipe(gulp.dest('./client/dist/module-patterns/es2015/'));
+const watch = require("gulp-watch");
+const exec = require('child_process').exec; // part of nodejs - no npm package needed
+const plumber = require("gulp-plumber");
+const del = require('del');
+
+gulp.task('clean', function (done) {
+    var toDelete = ["./client/dist"];
+    del(toDelete);
+    done();
 });
+
+gulp.task('transpile', () => {
+    return gulp.src('./client/src/**/*.js')
+        .pipe(plumber()) // plumber prevents errors breaking the watch
+        .pipe(babel({
+            presets: ["latest"]
+        }))
+        .pipe(gulp.dest('./client/dist/'));
+});
+
+gulp.task("copy", () => {
+    return gulp.src("./client/src/**/*.html")
+        .pipe(gulp.dest("./client/dist/"));
+})
+
+gulp.task("watch", gulp.series("clean", "transpile", "copy"), () => {
+    return watch("./client/src/**/*.js", () => gulp.start(["transpile", "copy"]));
+});
+
+gulp.task("liveserver", (done) => {
+    exec("live-server", (error) => { 
+        console.log(error);
+        done(); 
+    });
+});
+
+gulp.task("default", gulp.parallel("watch", "liveserver"), () => { });
