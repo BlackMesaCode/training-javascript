fb2493eea1979483d7d8537acd4a5f451a57af85
Stephan Sum <sum@live.de>
2017-03-15 00:15:11 +0100

    added infos on how to deal with extension methods

--- server/src/various/extensions.js
+++ server/src/various/extensions.js
@@ -1,3 +1,5 @@
+"use strict";
+
 // extension methods:
 
 String.prototype.calcLength = function() {
@@ -5,3 +7,16 @@ String.prototype.calcLength = function() {
 }
 
 console.log("hello".calcLength());
+
+
+Array.prototype.last = function() {
+    return this[this.length -1];
+}
+
+Array.prototype.last = () =>  {
+    console.log(this); // undefined due to "this" being resolved lexically
+    return this[this.length -1]; // wont work because "this" is undefined
+}
+
+var a1 = [ 1,2,3 ];
+console.log(a1.last()); // returns 3
